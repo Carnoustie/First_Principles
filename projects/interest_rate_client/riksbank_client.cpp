a4080d246d6fc1bcc6e6bf52d589791242e9fc21
osp <osp@kth.se>
2025-09-10 19:46:21 +0200

    added json parsing

--- projects/interest_rate_client/riksbank_client.cpp
+++ projects/interest_rate_client/riksbank_client.cpp
@@ -4,8 +4,6 @@
 
 
 #include <iostream>
-#include <openssl/ossl_typ.h>
-#include <openssl/tls1.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,23 +12,48 @@
 #include <string.h>
 #include <string>
 #include <unistd.h>
+#include <vector>
 
-
-//openssl headers for tls handshake with server
-#include <openssl/bio.h>
+//openssl header for TLS handshake with server
 #include <openssl/ssl.h>
-#include <openssl/err.h>
 
 using namespace std;
+
+vector<string> splitstr(string s,char c){
+    vector<string> output;
+
+    int start = 0;
+    int end = s.find(c,start+1);
+    string partial = s.substr(0, end-start-1);
+    output.push_back(partial);
+    
+    while(start>=0 && end>=0){
+        start = end;
+        end = s.find(c,start+1);
+        partial = s.substr(start+1, end-start-1);
+        output.push_back(partial);
+    }
+
+    return output;
+}
+
+
+void print_vector(vector<string> v){
+    cout << "\n\n\n\n\n\nVector contents:\n\n";
+    for(int i=0; i<v.size(); i++){
+        cout << "\n\n" << v.at(i);
+    }
+    cout << "\n\n";
+}
+
+
 
 int main(int argc, char* argv[]){
     cout << "\n\nInterest rate modeling!\n\n";
 
-    //Code to fetch interest rate data
-    
+    //Allocate memory for SSL objects for TLS handshake with server 
     SSL_CTX* ctx = NULL;
     SSL* ssl = NULL;
-    BIO* bio = NULL;
 
     ctx = SSL_CTX_new(TLS_client_method()); //create ctx-object to enable tls-handshake
     if(ctx==NULL){
@@ -50,26 +73,22 @@ int main(int argc, char* argv[]){
     exit(1);
     }
 
-    //Client here
-    string riksbank_ip_adress = "4.209.28.116"; //fetched by executing "nslookup api.riksbank.se" from terminal
-    //string port = ???;
-    //
-
-    
+    //Addrinfo struct to specify API IP-adress and client IP-adress
     struct addrinfo specs, *addr_list, *addr_candidate;
-    
-    memset(&specs, 0, sizeof(specs));
-    
+
+    memset(&specs, 0, sizeof(specs)); //Allocate memory for network protocol specs
     specs.ai_family = AF_INET;
     specs.ai_socktype = SOCK_STREAM;
     specs.ai_protocol = 0;
 
+    //Declare API details
     char* host = "api.riksbank.se";
     char* port = "443";
     char* path = "/swestr/v1/all/SWESTR?fromDate=2021-11-01";
 
-    int retaddrinfo = getaddrinfo(host, port, &specs, &addr_list);
+    int retaddrinfo = getaddrinfo(host, port, &specs, &addr_list); //Let kernel assign available ip-adresses
 
+    //Loop over available addresses until connection is established
     for(addr_candidate=addr_list; addr_candidate!= NULL; addr_candidate=addr_candidate->ai_next){
         cout << "\n\nhit loop\n\n";
         int sock_fd = socket(addr_candidate->ai_family, addr_candidate->ai_socktype, addr_candidate->ai_protocol);
@@ -79,17 +98,21 @@ int main(int argc, char* argv[]){
         }
         int connectret = connect(sock_fd, addr_candidate->ai_addr, addr_candidate->ai_addrlen);
 
-        cout << "\n\nconnect return: " << connectret << "\n\n";
-        
-
-
-        if(connectret==0){
-            cout << "\n\nsuccess connect!\n\n";
+        if(connectret!=0){
+            cout << "\n\nconnect fail!\n\n";
+            exit(1);
+        }else{
+            cout << "\n\nconnected to API server";
         }
-
-        string request = "GET /swestr/v1/all/SWESTR?fromDate=2021-11-01 HTTP/1.1\r\n";
         
-        request =
+        //SSL connect
+        int hook_to_fd = SSL_set_fd(ssl,sock_fd); //hook C-socket to SSL TLS-object
+        int SNI_info_to_server = SSL_set_tlsext_status_exts(ssl, host); //Tell server which hostname the client is targeting.
+        int sslConnect = SSL_connect(ssl); //SSL-connect with handshake
+        cout << "\n\nssl connect result: " << sslConnect << "\n\n";
+        
+        //HTTP v1.1 request
+        string request =
             "GET /swestr/v1/all/SWESTR?fromDate=2021-11-01 HTTP/1.1\r\n"
             "Host: api.riksbank.se\r\n"
             "User-Agent: plain-client/0.1\r\n"
@@ -97,33 +120,24 @@ int main(int argc, char* argv[]){
             "Connection: close\r\n"
             "\r\n";
 
-        int hook_to_fd = SSL_set_fd(ssl,sock_fd);
-        int SNI_info_to_server = SSL_set_tlsext_status_exts(ssl, host); //Tell server which hostname the client is targeting.
-        
-        int sslConnect = SSL_connect(ssl);
-
-        cout << "\n\nssl connect attempt: " << sslConnect << "\n\n";
-       
         // Make HTTP-request
         SSL_write(ssl, request.data(), (int)request.size());                                                                
-        //write(sock_fd, request.c_str(), request.size());
 
         char read_data_buffer[8192];
         ssize_t n_bytes;
 
-        string container;
-
+        string container; // collect API response here
         while( (n_bytes = SSL_read(ssl, read_data_buffer, sizeof(read_data_buffer)-1)) >0){
             read_data_buffer[n_bytes]='\0'; //zero-terminate buffer, else not printable to std-out
             container.append(read_data_buffer,n_bytes);
-            //cout.write(read_data_buffer,n_bytes);
-            //cout << "hit loop";
         }
 
-        //cout << "output: " << container;
 
-        string finaldata;
+        string finaldata; //remove response headers
+        string json_element; //for parsing json
+        vector<string> split_json; // for parsing json
 
+        //parsing json
         int start = container.find('{');
         int end = container.find('}');
         finaldata.append(container.substr(start+1, end-start-1));
@@ -132,11 +146,15 @@ int main(int argc, char* argv[]){
             end = container.find('}', start);
             if(start>0 && end >0){
                 finaldata.append("\n\n\n\n");
-                finaldata.append(container.substr(start+1, end-start-1));
+                json_element = container.substr(start+1, end-start-1);
+                split_json = splitstr(json_element, ',');
+                print_vector(split_json);
+                finaldata.append(json_element);
             }
         }
 
-        cout << "\n\n\n\n\n\nfinal output:\n\n\n\n\n\n" << finaldata << "\n\n";
+        //cout << "\n\n\n\n\n\nfinal output:\n\n\n\n\n\n" << finaldata << "\n\n";
+
         cout << flush;
     }
 
